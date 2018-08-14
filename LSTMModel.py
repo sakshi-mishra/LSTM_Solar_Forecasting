2dcfc6b38bd105833c7adb3170cd21f335c2b0f9
Sakshi Mishra <sakshi.m@outlook.com>
2018-08-13 21:50:24 -0600

    LSTM class implementation

---
+++ LSTMModel.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(LSTMModel, self).__init__()
+        #Hidden Dimension
+        self.hidden_dim = hidden_dim
+
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+
+        #Building the RNN
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, output_dim)
+
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initializing the hidden state with zeros
+        # (layer_dim, batch_size, hidden_dim)
+        h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
+
+        c0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
+
+
+        #One time step (the last one perhaps?)
+        out, (hn,cn) = self.lstm(x, (h0,c0))
+
+        # Indexing hidden state of the last time step
+        # out.size() --> ??
+        #out[:,-1,:] --> is it going to be 100,100
+        out = self.fc(out[:,-1,:])
+        # out.size() --> 100,1
+        return out
